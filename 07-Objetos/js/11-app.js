92a0dde8275fe6b095884cc3a7b1ea2c823d64c9
carmen Luisa <carmenluisa525@hotmail.com>
2025-06-09 19:38:44 +0100

    act

--- 07-Objetos/js/11-app.js
+++ 07-Objetos/js/11-app.js
@@ -4,7 +4,7 @@
     precio: 500,
     disponible: true,
     mostrarInfo function(){
-        return 'El producto: ${this.nombre]' tiene un precio de ${this.precio}';
+        return 'El producto: ${this.nombre}' tiene un precio de ${this.precio}';
     }
     
 }; */
@@ -14,7 +14,7 @@ const producto= {
     precio: 500,
     disponible: true,
     mostrarInfo(){
-        return 'El producto: ${this.nombre{' tiene un precio de ${this.precio}';
+        return 'El producto: ${this.nombre}' tiene un precio de ${this.precio}';
     }
 
 console.log(producto.mostrarInfo());
