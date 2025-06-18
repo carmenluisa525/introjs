0423741635618c46dd15822ddf32e7f3529695bc
carmen Luisa <carmenluisa525@hotmail.com>
2025-06-18 20:26:14 +0100

    ac

---
+++ 11-Iteradores/06-app.js
@@ -0,0 +1,15 @@
+
+let pendientes = ['tarea','comer','caminar'];
+
+//Usando el forEach
+pendientes.forEach((pendientes,index)) =>{
+    console.log(`${index}:${pendientes}`);
+}
+
+let pendientesConIndice = pendientes.map((pendientes, index))0>{
+    RETURN `${index}: ${pendientes}`;
+
+}
+pendientesConIndice.forEach(item =>{
+    console.log(item);
+})
