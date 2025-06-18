8c423f5e2bc6dee736953da0b406a40936248005
carmen Luisa <carmenluisa525@hotmail.com>
2025-06-18 20:03:51 +0100

    ac

--- 11-Iteradores/03-app.js
+++ 11-Iteradores/03-app.js
@@ -4,3 +4,32 @@
 //Si el número es múltiplo sólo de 3,imprimir Fizz
 //Si el número e múltiplo sólo de 5, imprimir Buzz
 //Si no es múltiplo de 3 ni de 5, imprimir el número
+
+for(let i=1; i<=50; i++){
+    if(i % 3 ==0 && i % 5 == 0){
+        console.log("FizzBuzz");
+    }else if(i % 3 ==0){
+        console.log("Fizz");
+    }else if(i % 5 == 0){
+        console.log("Buzz")
+    }else{
+        console.log(`Elnúmero ${i}`);
+    }
+    
+}
+
+
+for(let i=1; i<=50; i++){
+    let salida = ""
+    if(i%3==0){
+        salida += 'Fizz';
+    }
+    if (i % 5== 0){
+        salida +='Buzz';
+    }
+    if (salida === ""){
+        salida += `&{i}`;
+    }
+    
+    console.log(salida);
+}
---
+++ 11-Iteradores/04-app.js
@@ -0,0 +1,21 @@
+
+//Operador While
+let i = 0;
+while (i<10){
+    console.log(`Número ${i}`);
+    i++;
+}
+
+function generarNumerosAleatorios(){
+    let numeros = [];
+    const cantidad = 100;
+    while(numeros.length< cantidad){
+        //Aleatorio entre 1 y 1000
+        let numeroAleatorio =Math.floor( Math.random() * 1000) +1;
+        if(!numeros.includes(numeroAleatorio)){
+            numeros.push(numeroAleatorio);
+        }
+    }
+    console.log(numeros);
+}
+
